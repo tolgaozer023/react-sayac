699d106ff8b57bf8c438251d07c979f4f5575788
tolgaozer023 <88386789+tolgaozer023@users.noreply.github.com>
2022-07-14 18:37:47 +0300

    Sayac oluşturma tamamlandı
    - input alanı eklendi.
    - girilen input değerine göre artış ve azalış gerçekleştirildi.
    - CSS dosyası import edildi.

--- src/SayacScript.js
+++ src/SayacScript.js
@@ -1,18 +1,48 @@
 import React, { useState } from 'react'
+import "./sayacScript.css"
 
 function SayacScript() {
+ 
   const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(0);
+  
+  const handleChange = (event) => {
+    const {value}= event.target
+    setInputValue(parseInt(value))
+  }
+
+  const increase = () => {
+    let artisMiktari = 1
+    if(inputValue){
+      artisMiktari = inputValue
+    }
+    setCounter(counter+artisMiktari)
+  }
+
+  const decrease = () => {
+    let azalisMiktari = 1
+    if(inputValue){
+      azalisMiktari = inputValue
+    }
+    setCounter(counter-azalisMiktari)
+  }
+
   return (
     <>
-      <button className="counter-button" onClick={() => setCounter(counter + 1)}>
+      <button className="counter-button" onClick={increase}>
         +
       </button>
       <span className="counter">
         {counter}
       </span>
-      <button className="counter-button" onClick={() => setCounter(counter - 1)}>
+      <button className="counter-button" onClick={decrease}>
         -
       </button>
+
+      <label>Sayı giriniz:
+        <input value={inputValue ? inputValue : 0} onChange={handleChange}
+        />
+      </label>
     </>
   );
 }
